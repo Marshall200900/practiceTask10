61e023da9393a96e6ea051ad3ce3c12f71eff9d3
Арсений Сидоров <aasidorov_6@edu.hse.ru>
2019-06-24 10:17:32 +0500

    Добавьте файлы проекта.

---
+++ task10/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10
+{
+    class Program
+    {
+        static int Int(string sentence, int minBorder = int.MinValue, int maxBorder = int.MaxValue)
+        {
+            int result = 0;
+            bool ok = true;
+            do
+            {
+                Console.Write(sentence);
+                ok = int.TryParse(Console.ReadLine(), out result);
+                if (result < minBorder || result > maxBorder)
+                {
+                    ok = false;
+                }
+            }
+            while (!ok);
+            return result;
+        }
+        static int[] ParseNumbers(string sentence, int len)
+        {
+            int[] result = new int[0];
+            bool ok = true;
+            do
+            {
+                try
+                {
+                    Console.Write(sentence);
+                    string input = Console.ReadLine().Replace('.', ',');
+                    result = input.Trim().Split(' ').Select(x => int.Parse(x)).ToArray();
+                    if (result.Length != len) throw new IndexOutOfRangeException();
+                    ok = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Неверный формат строки");
+                    ok = false;
+
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Неверное количество чисел в строке");
+                    ok = false;
+                }
+
+            }
+            while (!ok);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int size = Int("Введите размер массива: ");
+
+            int[] array = ParseNumbers("Введите элементы массива через пробел: ", size);
+            Tree tree = new Tree(array);
+            tree.Run();
+        }
+    }
+}
---
+++ task10/Tree.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10
+{
+    class Node
+    {
+        public int Level;
+        public int Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+    class Tree
+    {
+        public Node Root;
+        public Tree(int[] array)
+        {
+
+            if (array.Length == 0) return;
+            Root = new Node(array[0]);
+            Node pointer = Root;
+            for (int i = 1; i < array.Length; i++)
+            {
+                while (pointer != null)
+                {
+                    if (array[i] < pointer.Value)
+                    {
+                        if (pointer.Left != null)
+                            pointer = pointer.Left;
+                        else
+                        {
+                            pointer.Left = new Node(array[i]);
+                            pointer = Root;
+                            break;
+                        }
+                    }
+                    else if (array[i] >= pointer.Value)
+                    {
+                        if (pointer.Right != null)
+                            pointer = pointer.Right;
+                        else
+                        {
+                            pointer.Right = new Node(array[i]);
+                            pointer = Root;
+                            break;
+                        }
+                    }
+
+                }
+
+            }
+        }
+        public void Run()
+        {
+            int level = 1;
+            Queue<Node> q = new Queue<Node>();
+            List<Node> list = new List<Node>();
+            Root.Level = level;
+            q.Enqueue(Root);
+            while (q.Count != 0)
+            {
+                Node x = q.Dequeue();
+                list.Add(x);
+                if (x.Left != null)
+                {
+                    x.Left.Level = x.Level + 1;
+                    level = x.Left.Level > level ? x.Left.Level : level;
+                    q.Enqueue(x.Left);
+                }
+                if (x.Right != null)
+                {
+                    x.Right.Level = x.Level + 1;
+                    level = x.Right.Level > level ? x.Right.Level : level;
+                    q.Enqueue(x.Right);
+                }
+            }
+            Console.WriteLine("Количество уровней: "+level);
+            for(int i = 1; i <= level; i++)
+            {
+                int num = (from c in list select c).Where(c => c.Level == i).Count();
+                Console.WriteLine($"Количество элементов на {i}-м ярусе: " + num);
+            }
+        }
+
+
+    }
+}
